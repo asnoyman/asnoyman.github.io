e2e52ed6a2309f4a2e5069ac0785d298799ca631
Adam Snoyman <adamsnoyman@gmail.com>
2025-06-10 17:54:08 +1000

    created skeleton for serpinsky

---
+++ javascript/serpinsky.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(800, 600);
+  noFill();
+  stroke(0);
+  triangle(width/2, height/2-200, width/2-200, height/2+200, width/2+200, height/2+200);
+}
+
+function draw() {
+  // background(135, 206, 235);
+
+  
+}
+
+function mousePressed() {
+  
+}
+
+function mouseReleased() {
+  
+}
