e6d344319273b8c7e6ce85bb0d4af477cc78ae41
Adam Snoyman <adamsnoyman@gmail.com>
2025-06-10 18:05:46 +1000

    Added first iteration of generation

--- javascript/serpinsky.js
+++ javascript/serpinsky.js
@@ -1,3 +1,12 @@
+let x1 = width/2;
+let y1 = height/2-200;
+let x2 = width/2-200;
+let y2 = height/2+200;
+let x3 = width/2+200;
+let y3 = height/2+200;
+let vertices = [{x: x1, y: y1}, {x: x2, y: y2}, {x: x3, y: y3}];
+
+
 function setup() {
   createCanvas(800, 600);
   noFill();
@@ -5,27 +14,37 @@ function setup() {
   triangle(width/2, height/2-200, width/2-200, height/2+200, width/2+200, height/2+200);
 }
 
-function draw() {
-  let x1 = width/2;
-  let y1 = height/2-200;
-  let x2 = width/2-200;
-  let y2 = height/2+200;
-  let x3 = width/2+200;
-  let y3 = height/2+200;
-  let randX = random(x1, x3);
-  let randY;
-  let r = random(1);
-  if (r < 0.5) {
-    randY = map(randX, x1, x2, y1, y2);
-  } else {
-    randY = map(randX, x2, x3, y2, y3);
-  }
-  let selectedVertex = random([{x: x1, y: y1}, {x: x2, y: y2}, {x: x3, y: y3}]);
-  point((randX + selectedVertex.x) / 2, (randY + selectedVertex.y) / 2);
-}
+// function draw() {
+//   let selectedVertex = random(vertices);
+
+//   // let randX = random(x1, x3);
+//   // let randY;
+//   // let r = random(1);
+//   // if (r < 0.5) {
+//   //   randY = map(randX, x1, x2, y1, y2);
+//   // } else {
+//   //   randY = map(randX, x2, x3, y2, y3);
+//   // }
+
+//   strokeWeight(4);
+//   point(selectedVertex.x, selectedVertex.y);
+//   // point((randX + selectedVertex.x) / 2, (randY + selectedVertex.y) / 2);
+// }
 
 function mousePressed() {
-  
+  let selectedVertex = random(vertices);
+
+  // let randX = random(x1, x3);
+  // let randY;
+  // let r = random(1);
+  // if (r < 0.5) {
+  //   randY = map(randX, x1, x2, y1, y2);
+  // } else {
+  //   randY = map(randX, x2, x3, y2, y3);
+  // }
+
+  strokeWeight(4);
+  point(selectedVertex);
 }
 
 function mouseReleased() {
